74f1bd911128cc4fca1bfc720498f99594ee2dc9
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2019-08-19 17:43:26 -0300

    Inicio do hello word

---
+++ packages/hello/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+@Controller()
+export class AppController {
+  constructor(
+    public appService: AppService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return 'test';
+  }e
+}
---
+++ packages/hello/src/app.ts
@@ -0,0 +1,20 @@
+import { AppModule } from './app.module';
+import { ServiceBuilder } from '@creativedc/lambda';
+
+const serviceBuilder: ServiceBuilder = new ServiceBuilder({
+  module: AppModule,
+  documentBuilder: {
+    title: '',
+    description: '',
+    version: '',
+    tag: '',
+  },
+  validation: true,
+  enableCors: true,
+});
+export const handler = serviceBuilder.handler;
+export const bootstrap = async () => {
+  const app: any = await serviceBuilder.bootstrapServer();
+  await app.listen(4000);
+  console.log("Server listen http://localhost:4000");
+};
